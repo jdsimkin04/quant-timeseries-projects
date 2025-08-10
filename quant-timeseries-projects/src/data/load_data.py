74039a1af144f40a977a3c84149fa01d2f581ae7
Jonathan Simkin <jdsimkin04@gmail.com>
2025-08-09 20:51:04 -0700

    get spy

---
+++ quant-timeseries-projects/src/data/load_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import yfinance as yf
+
+def get_spy_data(start= "2010-01-01", end="None") -> pd.DataFrame:
+    df = yf.download("SPY", start=start, end=end)
+    df = df[["Open", "High", "Low", "Close", "Volume"]]
+    df.dropna(inplace=True)
+    return df
+
+if __name__ == "__main__":
+    spy = get_spy_data()
+    print(spy.head())
